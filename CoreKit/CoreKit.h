862a5672f3cafac4888e689d6e8e7d7f2eb7c27d
Mostafa10399 <mostafaelshall972@gmail.com>
2024-03-29 17:51:48 +0200

    adding api key

---
+++ CoreKit/CoreKit.h
@@ -0,0 +1,18 @@
+//
+//  CoreKit.h
+//  CoreKit
+//
+//  Created by Mostafa Mahmoud on 29/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreKit.
+FOUNDATION_EXPORT double CoreKitVersionNumber;
+
+//! Project version string for CoreKit.
+FOUNDATION_EXPORT const unsigned char CoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreKit/PublicHeader.h>
+
+
---
+++ CoreKit/DTOs/Movies.swift
@@ -0,0 +1,8 @@
+//
+//  Movies.swift
+//  CoreKit
+//
+//  Created by Mostafa Mahmoud on 29/03/2024.
+//
+
+import Foundation
